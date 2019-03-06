b67c7b5ab6bf2adefbc00c436f6ce4c1217add67
Okall Vivian <vivieokall@gmail.com>
2019-03-06 11:55:41 +0300

    "Update settings.py"

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 from .forms import NewsLetterForm
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
 from .models import Article
 
@@ -14,9 +14,15 @@ def news_today(request):
         form = NewsLetterForm(request.POST)
         if form.is_valid():
             print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            HttpResponseRedirect('news_today')
     else:
         form = NewsLetterForm()
     return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
+
 
 def past_days_news(request, past_date):
     try:
